99ffe5bd4dab7690cb23873d85a6f4c5ceee324c
diosamuel <koding12sam@gmail.com>
2024-09-14 14:33:23 +0700

    feat: add eccomerce

--- src/pages/Dashboard/TambahKatalog.tsx
+++ src/pages/Dashboard/TambahKatalog.tsx
@@ -17,8 +17,8 @@ const FormLayout = () => {
   };
 
   const [files, setFiles] = useState([]);
-  const updateFiles = (incommingFiles) => {
-    setFiles(incommingFiles);
+  const updateFiles = (incomingFiles) => {
+    setFiles(incomingFiles);
   };
   const removeFile = (id) => {
     setFiles(files.filter((x) => x.id !== id));
@@ -27,17 +27,22 @@ const FormLayout = () => {
   const [submitLoading, setLoading] = useState<any>(false);
   const [formData, setFormData] = useState<any>({
     nama: '',
-    deskripsi:'',
+    deskripsi: '',
     kategori: '',
     stok: '',
     harga: '',
     diskon: '',
+    persenDiskon: '',
     warna: '',
-    berat: ''
+    berat: '',
+    lazada: '',
+    shopee: '',
+    tokopedia: '',
   });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
+    console.log(name,value)
     setFormData((prevData) => ({
       ...prevData,
       [name]: value
@@ -54,9 +59,12 @@ const FormLayout = () => {
     data.append('kategori', formData.kategori);
     data.append('stok', formData.stok);
     data.append('harga', formData.harga);
-    data.append('diskon', formData.diskon);
+    data.append('diskon', formData.harga-((formData.persenDiskon/100)*formData.harga));
     data.append('warna', formData.warna);
     data.append('berat', formData.berat);
+    data.append('lazada', formData.lazada);
+    data.append('shopee', formData.shopee);
+    data.append('tokopedia', formData.tokopedia);
     Array.from(files).forEach((file, index) => {
       data.append('img', file.file);
     });
@@ -117,7 +125,7 @@ const FormLayout = () => {
 
                 <div className="flex flex-col lg:flex-row lg:gap-6">
                   <div className="mb-4.5 w-full md:w-1/2">
-                    <label className="mb-2.5 block text-black dark:text-white">Harga Asli</label>
+                    <label className="mb-2.5 block text-black dark:text-white">Harga Asli Barang</label>
                     <input
                       type="number"
                       placeholder="Harga Barang"
@@ -130,18 +138,28 @@ const FormLayout = () => {
                   </div>
 
                   <div className="mb-4.5 w-full md:w-1/2">
-                    <label className="mb-2.5 block text-black dark:text-white">Harga Setelah Diskon</label>
+                    <label className="mb-2.5 block text-black dark:text-white">
+                      Diskon (dalam persen)
+                    </label>
                     <input
                       type="number"
-                      placeholder="Harga Barang"
+                      placeholder="Contoh: 50"
                       className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                       onChange={handleInputChange}
-                      name="diskon"
-                      value={formData.diskon}
+                      name="persenDiskon"
+                      value={formData.persenDiskon}
+                      max="100"
+                      min={0}
                       required
                     />
                   </div>
                 </div>
+                <div className="mb-4.5 w-full">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                      Harga Setelah Diskon {formData.persenDiskon}%
+                    </label>
+                    <p className='font-bold text-2xl text-white'>Rp{Number(formData.harga-((formData.persenDiskon/100)*formData.harga)).toLocaleString('id-ID')}</p>
+                  </div>
                 <div className="mb-4.5">
                   <label className="mb-2.5 block text-black dark:text-white">Stok</label>
                   <input
@@ -169,8 +187,7 @@ const FormLayout = () => {
                       className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary ${
                         isOptionSelected ? 'text-black dark:text-white' : ''
                       }`}
-                      required
-                    >
+                      required>
                       <option value="" disabled className="text-body dark:text-bodydark">
                         Pilih Kategori Barang
                       </option>
@@ -194,15 +211,13 @@ const FormLayout = () => {
                         height="24"
                         viewBox="0 0 24 24"
                         fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
+                        xmlns="http://www.w3.org/2000/svg">
                         <g opacity="0.8">
                           <path
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                            fill=""
-                          ></path>
+                            fill=""></path>
                         </g>
                       </svg>
                     </span>
@@ -218,34 +233,71 @@ const FormLayout = () => {
                     onChange={handleInputChange}
                     name="deskripsi"
                     value={formData.deskripsi}
-                    required
-                  ></textarea>
+                    required></textarea>
                 </div>
 
-                <div className="mb-4.5">
-                  <label className="mb-2.5 block text-black dark:text-white">Warna Barang</label>
-                  <input
-                    placeholder="Warna"
-                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                    onChange={handleInputChange}
-                    name="warna"
-                    value={formData.warna}
-                    required
-                  />
-                </div>
+                <div className="flex flex-col lg:flex-row lg:gap-6">
+                  <div className="mb-4.5 md:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">Warna Barang</label>
+                    <input
+                      placeholder="Warna"
+                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      onChange={handleInputChange}
+                      name="warna"
+                      value={formData.warna}
+                      required
+                    />
+                  </div>
 
-                <div className="mb-4.5">
-                  <label className="mb-2.5 block text-black dark:text-white">Berat Barang</label>
-                  <input
-                    placeholder="Berat"
-                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                    onChange={handleInputChange}
-                    name="berat"
-                    value={formData.berat}
-                    required
-                  />
+                  <div className="mb-4.5 md:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">Berat Barang</label>
+                    <input
+                      placeholder="Berat"
+                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      onChange={handleInputChange}
+                      name="berat"
+                      value={formData.berat}
+                      required
+                    />
+                  </div>
                 </div>
+                <div className="flex flex-col lg:flex-row lg:gap-6">
+                  <div className="mb-4.5 md:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">Link Lazada</label>
+                    <input
+                      placeholder="https://www.lazada.co.id/...."
+                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      onChange={handleInputChange}
+                      name="lazada"
+                      value={formData.lazada}
+                      required
+                    />
+                  </div>
 
+                  <div className="mb-4.5 md:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">Link Shopee</label>
+                    <input
+                      placeholder="https://shopee.co.id/..."
+                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      onChange={handleInputChange}
+                      name="shopee"
+                      value={formData.shopee}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4.5 md:w-1/2">
+                    <label className="mb-2.5 block text-black dark:text-white">Link Tokopedia</label>
+                    <input
+                      placeholder="https://www.tokopedia.com/...."
+                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      onChange={handleInputChange}
+                      name="tokopedia"
+                      value={formData.tokopedia}
+                      required
+                    />
+                  </div>
+
+                </div>
                 <div className="mb-6">
                   <label className="mb-2.5 block text-black dark:text-white">Gambar</label>
                   <div>
@@ -260,8 +312,7 @@ const FormLayout = () => {
                 <button
                   className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
                   type="submit"
-                  disabled={submitLoading}
-                >
+                  disabled={submitLoading}>
                   {submitLoading ? 'Sedang Upload...' : 'Tambah Katalog'}
                 </button>
               </div>
