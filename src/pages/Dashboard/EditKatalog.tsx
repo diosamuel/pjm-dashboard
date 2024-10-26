1e750b1b4049958b766cdfb32ea0e6def76d4759
diosamuel <koding12sam@gmail.com>
2024-10-26 09:13:09 +0700

    remove requird link

--- src/pages/Dashboard/EditKatalog.tsx
+++ src/pages/Dashboard/EditKatalog.tsx
@@ -319,7 +319,7 @@ const FormLayout = () => {
                       onChange={handleInputChange}
                       name="lazada"
                       value={formData.lazada}
-                      required
+                      
                     />
                   </div>
 
@@ -331,7 +331,7 @@ const FormLayout = () => {
                       onChange={handleInputChange}
                       name="shopee"
                       value={formData.shopee}
-                      required
+
                     />
                   </div>
                   <div className="mb-4.5 md:w-1/2">
@@ -344,7 +344,7 @@ const FormLayout = () => {
                       onChange={handleInputChange}
                       name="tokopedia"
                       value={formData.tokopedia}
-                      required
+
                     />
                   </div>
                 </div>
